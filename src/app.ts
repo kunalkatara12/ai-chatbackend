b430db184f446d3a10cec67297d43c8d21205af5
kunalkatara12 <kunalkatara888@gmail.com>
2023-12-29 15:34:58 +0530

    origin url changed

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET as string));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );

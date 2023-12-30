a16c63bc0538669899e0011a208d90a26eaac0cf
kunalkatara12 <kunalkatara888@gmail.com>
2023-12-30 11:46:53 +0530

    origin added *

--- src/app.ts
+++ src/app.ts
@@ -10,22 +10,21 @@ const app: Express = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET as string));
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: [
-//       "*",
-//       // "https://ai-chat-frontend.vercel.app/",
-//       // "https://ai-chat-frontend-git-master-kunalkatara12.vercel.app/",
-//       // "https://ai-chat-frontend-caukke0nc-kunalkatara12.vercel.app/",
-//       // "https://calm-khapse-f64d9a.netlify.app/",
-//       // "https://glittering-babka-72d1cb.netlify.app/",
-//     ],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: [
+      "*",
+      // "https://ai-chat-frontend.vercel.app/",
+      // "https://ai-chat-frontend-git-master-kunalkatara12.vercel.app/",
+      // "https://ai-chat-frontend-caukke0nc-kunalkatara12.vercel.app/",
+      // "https://calm-khapse-f64d9a.netlify.app/",
+      // "https://glittering-babka-72d1cb.netlify.app/",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 // remove this middleware in production
 // app.use(morgan("dev"));
 app.use("/api/v1", appRouter);
